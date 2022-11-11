76215ccd552e3fe9b8af9243a0ab1e5bf25392c6
ayowilfred95 <38457345+ayowilfred95@users.noreply.github.com>
2022-11-11 20:02:32 +0100

    make it better

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -8,7 +8,7 @@ export default function Marketplace() {
 const sampleData = [
     {
         "name": "NFT#1",
-        "description": "Alchemy's First NFT",
+        "description": "Wilfred's First NFT",
         "website":"http://axieinfinity.io",
         "image":"https://gateway.pinata.cloud/ipfs/QmXRJWYyTdwbxmn5jzRPnyoarSay5tmTdGXNKq13NsHpsb",
         "price":"0.03ETH",
@@ -17,7 +17,7 @@ const sampleData = [
     },
     {
         "name": "NFT#2",
-        "description": "Alchemy's Second NFT",
+        "description": "Wilfred's Second NFT",
         "website":"http://axieinfinity.io",
         "image":"https://gateway.pinata.cloud/ipfs/QmQiD9fAqVxeKVKPi1QnTtHA7mBEegnyPog5Q3dT5SCbCj",
         "price":"0.03ETH",
@@ -26,9 +26,36 @@ const sampleData = [
     },
     {
         "name": "NFT#3",
-        "description": "Alchemy's Third NFT",
+        "description": "Wilfred's Third NFT",
         "website":"http://axieinfinity.io",
         "image":"https://gateway.pinata.cloud/ipfs/QmSjWMo3EPKTqwDKbNmizaMtow9U4pBGYH3dPvNWYMtGxS",
+        "price":"0.03ETH",
+        "currentlySelling":"True",
+        "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
+    },
+    {
+        "name": "NFT#4",
+        "description": "Wilfred's Fourth NFT",
+        "website":"http://axieinfinity.io",
+        "image":"https://gateway.pinata.cloud/ipfs/QmVjz5WCvaJMMxvtzLgWSCRb8TGaTx4cvMe4cUmGihcBKb",
+        "price":"0.03ETH",
+        "currentlySelling":"True",
+        "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
+    },
+    {
+        "name": "NFT#5",
+        "description": "Wilfred's Fifth NFT",
+        "website":"http://axieinfinity.io",
+        "image":"https://gateway.pinata.cloud/ipfs/QmYyAC5MbvDFzqbvXK2dJMpzHzrGYWi3PoYdRtUvKQyVA2",
+        "price":"0.03ETH",
+        "currentlySelling":"True",
+        "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
+    },
+    {
+        "name": "NFT#5",
+        "description": "Wilfred's Sixth NFT",
+        "website":"http://axieinfinity.io",
+        "image":"https://gateway.pinata.cloud/ipfs/QmTuSFXoHEzxtahJAUDCFVJhoWc3yjVKYqgtnUBDAKbagh",
         "price":"0.03ETH",
         "currentlySelling":"True",
         "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
@@ -122,13 +149,28 @@ return (
                 })}
             </div>
         </div> 
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        
 
         <div >
-        <section className="bg-white dark:bg-gray-900 bg-no-repeatbg-auto hover:bg-contain container mx-auto bg-[url('/img/newsletter.jpeg')] ">
+        <section className=" bg-[url('/img/newsletter.jpeg')] bg-contain bg-auto bg-no-repeat bg-[length:1200px_350px] container mx-auto  ">
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
       <div className="mx-auto max-w-screen-md text-left sm:text-center">
           <h2 className="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl text-[#e5e5e5] dark:text-white">Stay in the Loop</h2>
-          <p className="mx-auto mb-8 max-w-2xl font-light text-gray-500 md:mb-12 sm:text-xl text-[#e5e5e5] dark:text-gray-400">Join our mailing list to stay in the loop with our newest feature releases, NFT drops, and tips for navigating Wilfred.</p>
+          <p className="mx-auto mb-8 max-w-2xl font-light text-gray-900 md:mb-12 sm:text-xl text-[#e5e5e5] dark:text-white">Join our mailing list to stay in the loop with our newest feature releases, NFT drops, and tips for navigating Wilfred.</p>
           <form action="#">
               <div className="items-center mx-auto mb-3 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
                   <div className="relative w-full">
@@ -140,10 +182,10 @@ return (
                       <input typeof="text" className="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter your email" type="email" id="email" required="" />
                   </div>
                   <div>
-                      <button className="rounded-none  bg-[#494947] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm" type="submit" class="py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-primary-700 border-primary-600 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Subscribe</button>
+                      <button className="rounded-full bg-[#725bdb] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm">Subscribe</button>
                   </div>
               </div>
-              <div class="mx-auto max-w-screen-sm text-sm text-left text-[#e5e5e5]  text-gray-500  newsletter-form-footer dark:text-gray-300">We care about the protection of your data. <a href="#" class="font-medium text-primary-600 dark:text-primary-500 hover:underline">Read our Privacy Policy</a>.</div>
+              <div class="mx-auto max-w-screen-sm text-sm text-left text-[#ffffff]  text-gray-500  newsletter-form-footer dark:text-gray-300">We care about the protection of your data. <a href="#" class="font-medium text-primary-600 dark:text-primary-500 hover:underline">Read our Privacy Policy</a>.</div>
           </form>
       </div>
   </div>
